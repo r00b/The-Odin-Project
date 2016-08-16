82ded861e81543f3023a03866ec35f6b003ac80d
Robert Steilberg <rsteilberg@gmail.com>
2016-08-16 17:37:29 -0400

    update README

---
+++ Rails-Blogger/app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+  before_filter :require_login, except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' has been created."
+    redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "The article '#{@article.title}' has been updated."
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' has been deleted."
+    redirect_to articles_path
+  end
+end
